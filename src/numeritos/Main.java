6d178fd06b2914f56d136cf3c5278f6e51972098
TecXarT by TcX <128599895+tecxion@users.noreply.github.com>
2025-06-06 09:45:17 +0200

    Update Main.java
    modificación del java añadiendo comentarios.

--- src/numeritos/Main.java
+++ src/numeritos/Main.java
@@ -10,21 +10,16 @@ public class Main {
 		// Variables.
 		Scanner sc = new Scanner(System.in);
 		int numUsuario = 0; //Iniciamos la variable en 0.
-		
+
+		// Inicio del programa
 		System.out.println("Inserta un número: ");
 		try {
-			numUsuario = Integer.parseInt(sc.nextLine());
-			int suma = numUsuario + 2;
-			System.out.println("El número dado es: " + numUsuario + ", pero si le sumas 2, se queda en " + suma);
-		}catch (Exception e) {
+			numUsuario = Integer.parseInt(sc.nextLine());  //Solicitamos el número.
+			int suma = numUsuario + 2; // Realizamos la suma
+			System.out.println("El número dado es: " + numUsuario + ", pero si le sumas 2, se queda en " + suma); //Imprimimos por pantalla el número y la suma
+		}catch (Exception e) { // Controlamos que el usuario no introduzca un dato no válido
 			System.out.println("No has introducido un número válido.");
 		}
-		
-		
-		
-		
-		
-		
 
 	}
 
